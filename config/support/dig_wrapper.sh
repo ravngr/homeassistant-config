2337314a43456a9fc161bdbfe5720fc30b6161c6
Chris Harrison <629204+ravngr@users.noreply.github.com>
2024-07-02 09:14:53 +1000

    Added/fixed dig wrapper script

---
+++ config/support/dig_wrapper.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# set -x
+
+# Default options to truncate output
+dig_options=("+stats" "+nocmd" "+nocomments" "+noquestion" "+time=2" "+tries=1" "+fail")
+
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <domain> <record_type> <server> [options, ...]"
+    exit 1
+fi
+
+domain="${1}"
+shift
+record_type="${1}"
+shift
+server="${1}"
+shift
+
+options=("${dig_options[@]}" "$@")
+
+dig_output=$(dig \
+    "@${server}" \
+    "${domain}" \
+    "${record_type}" \
+    ${options[@]} \
+)
+
+if [[ $? -ne 0 ]]; then
+    echo "unavailable"
+    exit 0
+fi
+
+# Check if record name is in output (record found)
+if echo "${dig_output}" | egrep -q "^${domain//./\\.}\."; then
+    echo "${dig_output}" | awk '/Query time:/ {print $4}'
+else
+    echo "unknown"
+fi
